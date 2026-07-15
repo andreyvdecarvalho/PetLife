0135642d88027c5ac1ac3fc31a423dfbe8297c14
Andrey <andreycarvalhodevict@policiamilitar.sp.gov.br>
2026-07-15 13:01:44 -0300

    fix(backend): reverte JacksonJsonMessageConverter para Jackson2 e suprime deprecation

--- apps/backend/src/main/java/com/petlife/modules/notification/infrastructure/config/RabbitConfig.java
+++ apps/backend/src/main/java/com/petlife/modules/notification/infrastructure/config/RabbitConfig.java
@@ -5,7 +5,7 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,7 +32,8 @@ public class RabbitConfig {
     }
 
     @Bean
-    public JacksonJsonMessageConverter jacksonJsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+    @SuppressWarnings("deprecation")
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
